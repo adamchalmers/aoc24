a1b4a906bf18485a83ff9b3d1b62fb98935fd678
Adam Chalmers <adam.s.chalmers@gmail.com>
2024-12-02 09:17:41 -0600

    Day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,23 +1,34 @@
 #![feature(array_windows)]
 
-use std::{ops::Not, str::FromStr};
-
-const TEST_INPUT: &str = "\
-7 6 4 2 1
-1 2 7 8 9
-9 7 6 2 1
-1 3 2 4 5
-8 6 4 4 1
-1 3 6 7 9
-";
+use std::str::FromStr;
 
 type Level = i64;
 type Report = Vec<Level>;
+
+fn parse_input(input: &str) -> Vec<Report> {
+    input.lines().map(parse_report).collect()
+}
 
 fn parse_report(line: &str) -> Report {
     line.split_whitespace()
         .map(|chars| Level::from_str(chars).unwrap())
         .collect()
+}
+
+fn is_report_safe_with_dampener(report: &Report) -> bool {
+    if is_report_safe(report) {
+        return true;
+    }
+
+    // Dampen by removing one item.
+    (0..report.len()).any(|i| is_report_safe(&dampen(report, i)))
+}
+
+// Dampen item i of the report.
+fn dampen(report: &Report, i: usize) -> Report {
+    let mut dampened = report.clone();
+    dampened.remove(i);
+    dampened
 }
 
 fn is_report_safe(report: &Report) -> bool {
@@ -30,21 +41,18 @@ fn is_report_safe(report: &Report) -> bool {
     (increasing || decreasing) && size_ok
 }
 
-fn parse_input(input: &str) -> Vec<Report> {
-    input.lines().map(parse_report).collect()
-}
-
 fn main() {
     let input = std::fs::read_to_string("input").unwrap();
     let reports = parse_input(&input);
-    for report in &reports {
-        assert!(report.is_empty().not())
-    }
     let q1 = reports
         .iter()
         .filter(|report| is_report_safe(report))
         .count();
-    println!("Q1: {q1}");
+    let q2 = reports
+        .iter()
+        .filter(|report| is_report_safe_with_dampener(report))
+        .count();
+    println!("Q1: {q1}\nQ2: {q2}");
 }
 
 #[cfg(test)]
@@ -54,13 +62,29 @@ mod tests {
     #[test]
     fn test_q1() {
         let reports = parse_input(TEST_INPUT);
-        for report in &reports {
-            assert!(report.is_empty().not())
-        }
         let q1 = reports
             .iter()
             .filter(|report| is_report_safe(report))
             .count();
         assert_eq!(q1, 2);
     }
+
+    #[test]
+    fn test_q2() {
+        let reports = parse_input(TEST_INPUT);
+        let q1 = reports
+            .iter()
+            .filter(|report| is_report_safe_with_dampener(report))
+            .count();
+        assert_eq!(q1, 4);
+    }
+
+    const TEST_INPUT: &str = "\
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+";
 }
