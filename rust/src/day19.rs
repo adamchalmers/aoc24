4053554d8d6212b608b4af40e9951eea822c7e5e
Adam Chalmers <adam.s.chalmers@gmail.com>
2024-12-31 10:42:17 +1100

    Day 19 part 2

--- rust/src/day19.rs
+++ rust/src/day19.rs
@@ -18,57 +18,65 @@ fn parse(input: &str) -> Input {
     Input { available, targets }
 }
 
-fn can_make(target: &str, available: &HashSet<String>) -> bool {
-    // Is it possible to make the prefix of `target` of length n?
-    // Trivially true for n=0.
-    let mut possible = vec![true];
-    // Easy to check for n=1.
-    possible.push(available.contains(&target[..1]));
+fn ways_to_make(target: &str, available: &HashSet<String>) -> usize {
+    // Definition: the `n-prefix` of a string is its prefix of length `n`.
+    //
+    // How many ways can you make the n-prefix of `target`?
+    // Only 1 way to make the empty string (0-prefix).
+    let mut possible = vec![1];
 
-    'n: for n in 2..=target.len() {
-        let subtarget = &target[..n];
-        assert_eq!(subtarget.len(), n);
-        println!("Checking {subtarget}");
-        if available.contains(subtarget) {
-            println!("\t{subtarget} is available");
-            possible.push(true);
-            continue;
+    // Only 1 way at most to make the 1-prefix.
+    possible.push(if available.contains(&target[..1]) {
+        1
+    } else {
+        0
+    });
+
+    for n in 2..=target.len() {
+        let n_prefix = &target[..n];
+        let mut ways_to_make = 0;
+
+        // One easy way to make this n-prefix is to use it directly from our available strings.
+        if available.contains(n_prefix) {
+            ways_to_make += 1;
         }
-        // Check all ways to make subtarget.
-        // Find them by making sequences of (n-1) <> 1, (n-2) <> 2, ..., 1 <> (n-1)
-        for rhs_len in 1..n {
-            let lhs_len = n - rhs_len;
-            assert_eq!(lhs_len + rhs_len, n);
-            let lhs = &subtarget[..lhs_len];
-            let rhs = &subtarget[lhs_len..];
-            println!("\t{lhs}<>{rhs}");
-            if possible[lhs_len] && available.contains(rhs) {
-                println!("\tWe can make {lhs} and {rhs} is available!");
-                possible.push(true);
-                continue 'n;
+
+        // Other ways to make this n-prefix:
+        //  - Make the 1-prefix and find the remaining suffix of length (n-1) in available
+        //  - Make the 2-prefix and find the remaining suffix of length (n-2) in available
+        //  - etc etc
+        //  - Make the (n-1) prefix and find the remaining suffix of length 1 in available
+        for suffix_len in 1..n {
+            let prefix_len = n - suffix_len;
+            let suffix = &n_prefix[prefix_len..];
+            if possible[prefix_len] > 0 && available.contains(suffix) {
+                // Because there's `k` ways to make the smaller prefix,
+                // and one way to make the smaller suffix,
+                // there's also `k` ways to make this n-prefix.
+                ways_to_make += possible[prefix_len];
             }
         }
-        println!("\tNot possible");
-        possible.push(false);
+        possible.push(ways_to_make);
     }
     possible[target.len()]
 }
 
 #[aoc(day19, part1)]
 fn q1(input: &Input) -> usize {
-    println!("Trying all {}", input.targets.len());
-    println!("using {} available patterns", input.available.len());
     input
         .targets
         .iter()
-        .enumerate()
-        .filter(|(i, target)| {
-            println!("{i:3}: {target}");
-            let b = can_make(target, &input.available);
-            println!("     {b}");
-            b
-        })
+        .filter(|target| ways_to_make(target, &input.available) > 0)
         .count()
+}
+
+#[aoc(day19, part2)]
+fn q2(input: &Input) -> usize {
+    input
+        .targets
+        .iter()
+        .map(|target| ways_to_make(target, &input.available))
+        .sum()
 }
 
 #[cfg(test)]
@@ -89,7 +97,7 @@ bwurrg
 brgr
 bbrgwb",
         );
-        let expected = 6;
-        assert_eq!(q1(&input), expected);
+        assert_eq!(q1(&input), 6);
+        assert_eq!(q2(&input), 16);
     }
 }
